eaea4b87fbe580f9c5e61d837b1435ee0ed4b891
shreyans-vm <108785153+shreyans-vm@users.noreply.github.com>
2022-11-16 15:15:12 +0530

    ques_1.py
    added a new file

---
+++ live_coding_questions/ques_1.py
@@ -0,0 +1,9 @@
+"""
+Write 3 classes - Derived class, base_1 class, and base_2 class. 
+The derived class should inherit base_1 and base_2 classes.
+Any derived class object created should be initiated with 2 variables - a,b.
+Any base_1 class object created should be initiated with variable a and base_2 class object should be initiated with variable b.
+On creating a base_1 class object, a new variable c should be attributed to the object with the value 5*a.
+On creating a base_2 class object, a new variable d should be attributed to the object with the value 10*b.
+Write a python program to create a derived class object with a=2,b=3 and print the 2 new variables (c,d) created.
+"""
